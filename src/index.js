c4238930614ecb6430bb6d088ae8f58d15e6a8b6
Isaías Fonseca <1isaiasfonseca@gmail.com>
2022-10-26 11:06:03 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Fernando';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-saludar( nombre );
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearTodoHtml);
+
+console.log('todos', todoList.todos);
+
+
+
+
+
+
+
+/*
+   La unica diferencia entre el Session Storage y el Local Storage, 
+   es que el Session Storage se va a borrar todo cuando cerremos el navegador web.
+   El localStorage no posee tiempo de expiracion, aqui vamos a poder almacenar info_
+   que necesite nuestra app.
+   ADVERTENCIA: la informacion almacenada en el localStorage es vivisble para el usuario final,
+    por lo tanto no es nada recomendable almacenar contraseñas o cosas sensibles para el ususario
+*/
+
+
+
